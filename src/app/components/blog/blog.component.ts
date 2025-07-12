6279e938dbd71c41437e7cca10632e7e60f44790
Antony Martinez <antony11995@hotmail.com>
2025-07-12 14:14:22 +0200

    Aplicando diseño inicial

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IEntradaBlog } from '../../interfaces/ientrada-blog.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-blog',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
